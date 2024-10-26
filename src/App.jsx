0bac26729875f7a445da53dfbc8fafb7f921830d
Marwa Jawad <marwajawad@Marwas-MacBook-Pro.local>
2024-10-26 12:37:17 +0400

    adding the edit functionality

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,10 @@ import NewTaskModal from "./newTaskModal";
 function App() {
   // state for show/hide the modal
   const [showModal, setShow] = useState(false);
-
   const [taskName, setTaskName] = useState("");
+
   const [tasks, setTasks] = useState([]);
+  const [selectedItem, setSelectedItem] = useState("");
 
   function addTask() {
     let myTasks = [...tasks];
@@ -24,15 +25,30 @@ function App() {
     // filter the array base on the id
     // kick the item of that id out
     let filterdTasks = tasks.filter((item) => item.id != id);
-
     // update tasks state using setTasks
     setTasks(filterdTasks);
+  }
+  function onSave(id) {
+    let updateTasks = tasks.map((item) => {
+      if (id == item.id) {
+        return { ...item, name: selectedItem };
+      } else {
+        return item;
+      }
+    });
+    setTasks(updateTasks);
   }
   return (
     <div>
       <button onClick={() => setShow(true)}>+</button>
 
-      <List tasks={tasks} deletItem={deletItem} />
+      <List
+        onSave={onSave}
+        selectedItem={selectedItem}
+        setSelectedItem={setSelectedItem}
+        tasks={tasks}
+        deletItem={deletItem}
+      />
 
       <NewTaskModal
         showModal={showModal}
--- src/list.jsx
+++ src/list.jsx
@@ -1,20 +1,45 @@
+import { useState } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
-import { FaRegTrashAlt } from "react-icons/fa";
+import { FaRegTrashAlt, FaEdit } from "react-icons/fa";
+import EditTaskModal from "./editTaskModal";
 
-function List({ tasks, deletItem }) {
+function List({ tasks, deletItem, onSave, selectedItem, setSelectedItem }) {
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [selectedId, setSelectedId] = useState(null);
   return (
-    <ListGroup>
-      {tasks.map((item) => (
-        <ListGroup.Item>
-          <p>{item.name}</p>
-          <FaRegTrashAlt
-            size={20}
-            color="red"
-            onClick={() => deletItem(item.id)}
-          />
-        </ListGroup.Item>
-      ))}
-    </ListGroup>
+    <div>
+      <ListGroup>
+        {tasks.map((item) => (
+          <ListGroup.Item>
+            <p>{item.name}</p>
+            <FaRegTrashAlt
+              size={20}
+              color="red"
+              onClick={() => deletItem(item.id)}
+            />
+            <FaEdit
+              onClick={() => {
+                setShowEditModal(true);
+                setSelectedItem(item.name);
+                setSelectedId(item.id);
+              }}
+              size={20}
+              color="green"
+              className="mx-3"
+            />
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+
+      <EditTaskModal
+        onSave={onSave}
+        selectedId={selectedId}
+        selectedItem={selectedItem}
+        setSelectedItem={setSelectedItem}
+        showEditModal={showEditModal}
+        setShowEditModal={setShowEditModal}
+      />
+    </div>
   );
 }
 
